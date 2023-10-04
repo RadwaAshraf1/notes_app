c86691fb2e74178a822aafb4c85074f43362adba
RadwaAshraf1 <radwaashrafamged@gmail.com>
2023-10-05 00:17:44 +0300

    edit custom app bar and custom text field

--- lib/views/widgets/custom_app_bar.dart
+++ lib/views/widgets/custom_app_bar.dart
@@ -2,17 +2,23 @@ import 'package:flutter/material.dart';
 import 'custom_search_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
+  const CustomAppBar({super.key, required this.title, required this.icon});
 
+  final String title;
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
-    return const Row(
-          children: [
-            Text("Notes",style: TextStyle(fontSize: 26),),
-            Spacer(),
-            CustomSearchIcon(),
-          ],
-        );
-    
+    return  Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(fontSize: 26),
+        ),
+        CustomIcon(
+          icon: icon,
+        ),
+      ],
+    );
   }
 }
--- lib/views/widgets/edite_note_view_body.dart
+++ lib/views/widgets/edite_note_view_body.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:notesapp/views/widgets/custom_app_bar.dart';
+import 'package:notesapp/views/widgets/custom_text_field.dart';
 
 class EditeNoteViewBody extends StatelessWidget {
   const EditeNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 26),
+      child: Column(
+        children: [
+          SizedBox(height: 50,),
+          CustomAppBar(title: 'Edit Note', icon: Icons.check),
+           SizedBox(height: 50,),
+          CustomTextField(hint: 'Title'),
+            SizedBox(height: 16),
+            CustomTextField(
+              hint: 'Content',
+              maxLines: 5,
+            ),
+        ],
+      ),
     );
   }
 }
